67982b9b2ce2432d76823b924040362cd0f7a319
Khasan <xasan2006@gmail.com>
2015-05-25 17:43:12 +0300

    added files

---
+++ VideoDemo/VideoDemo.iOS/Renderers/StartPageRenderer.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms;
+using CoreGraphics;
+using AVFoundation;
+
+[assembly: ExportRenderer(typeof(VideoDemo.StartPage), typeof(VideoDemo.iOS.Renderers.StartPageRenderer))]
+namespace VideoDemo.iOS.Renderers
+{
+    public class StartPageRenderer : PageRenderer
+    {
+
+        PlayerViewController playerController;
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();            
+            var view = NativeView;
+
+            playerController = new PlayerViewController();
+            view.AddSubview(playerController.View);
+
+            view.ConstrainLayout(() => this.playerController.View.Top() == view.Top() &&
+                                        this.playerController.View.Left() == view.Left() &&
+                                        this.playerController.View.Right() == view.Right() &&
+                                        this.playerController.View.Bottom() == view.Bottom());
+        }
+
+       
+    }
+}
