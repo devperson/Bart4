7e956d5a41cd57bc7eb7960b43668a8edfc7901a
Khasan <xasan2006@gmail.com>
2015-05-26 13:45:41 +0300

    added missing file

---
+++ VideoDemo/VideoDemo/StartPage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace VideoDemo
+{
+    public class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            this.Content = new Label { Text = "Hello", HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, TextColor = Color.White };
+        }
+    }
+}
